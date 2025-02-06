dc19152fec273412b0e8f4b56ec4ef216a0b26c8
NaGyeong Choi <83863325+imgyeongx-x@users.noreply.github.com>
2025-02-06 17:07:23 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,35 +1,15 @@
 
 #include <iostream>
+#include <numeric>
+
 using namespace std;
 
 int main() {
 
 	int a, b;
-
 	cin >> a >> b;
 
-	int N = a;
-	if (a > b)
-		N = b;
-
-	for (int i = b; i >= 1; i--) {
-		if (a % i == 0 && b % i == 0) {
-			N = i;
-			break;
-		}
-	}
-
-	int M = N;
-
-	while (true) {
-		if (M % a == 0 && M % b == 0) {
-			break;
-		}
-		M += N;
-	}
-
-	cout << N << "\n" << M;
-
+	cout << gcd(a, b) << "\n" << lcm(a, b);
 
 	return 0;
 }
