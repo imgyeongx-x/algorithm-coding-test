acb680248c8dc3f145bd4583cd5c2a33fd10acda
NaGyeong Choi <83863325+imgyeongx-x@users.noreply.github.com>
2025-02-09 13:23:19 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    for (int i = 0; i < numbers.size(); i++){
+        for (int j = i+1; j < numbers.size(); j++){
+            if (numbers[i] * numbers[j] > answer)
+                answer = numbers[i] * numbers[j];
+        }
+    }
+    return answer;
+}
