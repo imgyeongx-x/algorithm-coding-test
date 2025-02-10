d2dc8227dfdf608e5f4b9f5bff0ec446786e8a5d
NaGyeong Choi <83863325+imgyeongx-x@users.noreply.github.com>
2025-02-10 11:44:43 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> dots) {
+    double slope1 = (double)(dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]);
+    double slope2 = (double)(dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]);
+    
+    if (slope1 == slope2)
+        return 1;
+    
+    slope1 = (double)(dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]);
+    slope2 = (double)(dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]);
+    
+    if (slope1 == slope2 && (dots[2][0] - dots[0][0]) != 0 && (dots[3][0] - dots[1][0]) != 0)
+        return 1;
+    
+    slope1 = (double)(dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]);
+    slope2 = (double)(dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]);
+    
+    if (slope1 == slope2)
+        return 1;
+    
+    return 0;
+}
