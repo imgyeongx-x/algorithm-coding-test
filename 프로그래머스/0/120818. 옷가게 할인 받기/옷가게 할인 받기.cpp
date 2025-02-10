40a9df20b8c10b7fdfd4e4b9cae5fd355eb07cbd
NaGyeong Choi <83863325+imgyeongx-x@users.noreply.github.com>
2025-02-10 12:08:06 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = price >= 500000 ? price * 0.8 
+        : price >= 300000 ? price * 0.9 
+            : price >= 100000 ? price * 0.95 : price;
+    return answer;
+}
