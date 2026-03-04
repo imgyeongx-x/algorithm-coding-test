ea6ba610f7588aa923bc8ee59bb0b2a844e8093e
choing <83863325+imgyeongx-x@users.noreply.github.com>
2026-03-04 22:27:08 +0900

    [Silver III] Title: 1로 만들기, Time: 496 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,18 @@
+# 백준 1463번
+
+# 풀이 알고리즘: Dynamic Programming
+
+N = int(input())
+
+dp = [0] * (N+1)
+
+for i in range(2, N+1):
+  dp[i] = dp[i-1] + 1
+
+  if i % 2 == 0:
+    dp[i] = min(dp[i//2]+1, dp[i])
+
+  if i % 3 == 0:
+    dp[i] = min(dp[i//3]+1, dp[i])
+
+print(dp[N])
