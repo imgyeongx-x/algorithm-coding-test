9b51205fce8f18d2542b4449f3a2880b45f095d1
choing <83863325+imgyeongx-x@users.noreply.github.com>
2026-03-15 00:34:06 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 4052 ms, Memory: 158324 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+li = list(map(int, input().split()))
+
+bs = []
+bs.append(float("-inf"))
+bs.append(li[0])
+
+for i in range(1, N):
+  if bs[-1] < li[i]:
+    bs.append(li[i])
+  else:
+    st = 0
+    ed = len(bs)
+    while (st < ed):
+      mid = (st+ed)//2
+      if bs[mid] < li[i]:
+        st = mid + 1
+      else:
+        ed = mid
+    bs[ed] = li[i]
+  
+print(len(bs)-1)
