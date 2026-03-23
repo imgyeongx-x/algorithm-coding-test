07cca1ec8675e0c1906287d0516acf65440e4541
choing <83863325+imgyeongx-x@users.noreply.github.com>
2026-03-23 17:59:16 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+for i in range(n, 0, -1):
+  for j in range(0, n-i):
+    print(" ", end="")
+  for j in range(0, i):
+    print("*", end="")
+  for j in range(0, i-1):
+    print("*", end="")
+  print()
+for i in range(2, n+1):
+  for j in range(0, n-i):
+    print(" ", end="")
+  for j in range(0, i):
+    print("*", end="")
+  for j in range(0, i-1):
+    print("*", end="")
+  print()
